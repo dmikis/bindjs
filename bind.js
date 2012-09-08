9422df964675b7a13803ec177a368afe7c359986
Kirill Dmitrenko <kdmitrenko@gmail.com>
2012-09-08 13:48:34 +0400

    More perfomance %)

--- bind.js
+++ bind.js
@@ -3,28 +3,27 @@
     var allArgsPlaceholder = {__isPlaceholder: true};
 
     function bind(fn, ctx, arg1 /*, ... */) {
-        var args = Array.prototype.slice.call(arguments, 2),
-            allPlaceholderIndex = args.indexOf(allArgsPlaceholder),
-            placeholdersIndexes = args
+        var args = Array.prototype.slice.call(arguments, 2);
+
+        var concatCallString = ['(',
+            'Array.prototype.concat.apply([],[',
+            args
                 .map(function (arg, i) {
-                    return Object(arg).__isPlaceholder ? i : -1;
+                    if (Object(arg).__isPlaceholder) {
+                        return isNaN(arg.__i) ?
+                            'Array.prototype.slice.call(wrapperArgs)' :
+                            'wrapperArgs[' + arg.__i + ']';
+                    } else {
+                        return 'args[' + i + ']';
+                    }
                 })
-                .filter(function (index) { return index >= 0; });
+                .join(),
+        ']))'].join('');
 
         return function () {
-            var localArgs = args.slice(),
-                wrapperArgs = arguments;
+            var wrapperArgs = arguments;
 
-            placeholdersIndexes.forEach(function (i) {
-                localArgs[i] = wrapperArgs[localArgs[i].__i];
-            });
-
-            if (allPlaceholderIndex >= 0) {
-                localArgs.splice.apply(localArgs,
-                    Array.prototype.concat.apply([allPlaceholderIndex, 1], wrapperArgs));
-            }
-
-            return fn.apply(ctx, localArgs);
+            return fn.apply(ctx, eval(concatCallString));
         };
     }
 
